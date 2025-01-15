0089a10165c8b501dc8f6f1669d7b583b2041d67
boui <119169800+bouibot@users.noreply.github.com>
2025-01-15 19:43:39 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -9,26 +9,6 @@ local stringSplit, stringFind, stringSub = string.split, string.find, string.sub
 -- Requests:
 local allowedMethods = {"GET", "POST"};
 
-function getUrlData(s)
-    local split = stringSplit(str, "://");
-    local protocol = split[1];
-    local slashFound = stringFind(split[2], "/");
-
-    local hostnport, path = split[2], "/";
-    if slashFound then
-        hostnport = stringSub(split[2], 1, slashFound-1);
-        path = stringSub(split[2], slashFound, -1);
-    end;
-    
-    local host, port = hostnport, "80";
-    if stringFind(hostnport, ":") then
-        split = stringSplit(hostnport, ":");
-        host = split[1];
-        port = split[2];
-    end;
-    return protocol, host, port, path;
-end;
-
 getgenv().request = newcclosure(function(data: Table)
     assert(type(data) == "table", "table expected");
     local url = data.Url;
@@ -38,11 +18,13 @@ getgenv().request = newcclosure(function(data: Table)
     assert(method, "'Method' field is undefined");
     assert(tableFind(allowedMethods, method), `'Method' field is incorrect; only {tableConcat(allowedMethods, ",")} allowed!`);
 
-    local protocol, host, port, path = getUrlData(url);
-
+    local useFunction;
     if method == "GET" then
-        return httpget(host, path, port);
+		useFunction = httpget;
     elseif method == "POST" then
-		return httppost(host, path, port);
-    end;
+		useFunction = httppost;
+	end;
+
+    local data, code, message = useFunction(url);
+    return {Body = data, Success = (message == nil), Message = message, StatusCode = code};
 end);
